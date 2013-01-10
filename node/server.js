f11e7b4aca4f209e1558bdc624a34423dd2ba827
Attila Miklosi <attila.miklosi@gmail.com>
2013-01-10 18:30:39 +0000

    removed unused LTI specific code

--- node/server.js
+++ node/server.js
@@ -11,26 +11,6 @@ var scores = {};
 
 app.use(express.static(__dirname + '/'));
 
-app.post("/grade", function (req, res) {
-
-    req.on('data', function (chunk) {
-        console.log("Receiving Post:");
-        var xml = chunk.toString();
-        var sid = xml.match("<sourcedId>(.*?)</sourcedId>")[1];
-        var score = xml.match("<textString>(.*?)</textString>")[1];
-        console.log(sid);
-        console.log(score);
-        scores[sid] = score;
-    });
-
-    res.send("Okay");
-});
-
-app.get("/grade", function (req, res) {
-    console.log(req.query);
-    res.send(scores[req.query.id]);
-});
-
 port = process.env.PORT || 8000;
 
 app.listen(port, function () {
